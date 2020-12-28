b8945f837749aa5eca1ee6c8bb1ccb949636b4d6
vlad chub <vlad.chub@gmail.com>
2020-12-27 20:42:47 -0800

    add homework

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-console.log("hello from index.js");
+console.log('hello from index.js');
 
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
 const X = canvas.width;
 const Y = canvas.height;
 var x = X / 2;
@@ -13,7 +13,7 @@ var dy = -2;
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, r, 0, Math.PI * 2);
-  ctx.fillStyle = "#0095DD";
+  ctx.fillStyle = '#0095DD';
   ctx.fill();
   ctx.closePath();
 }
@@ -21,17 +21,16 @@ function drawBall() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   if (x + r > X || x - r < 0) {
-    dx = -dx
+    dx = -dx;
   }
 
   if (y + r > Y || y - r < 0) {
-    dy = -dy
+    dy = -dy;
   }
 
   drawBall();
   x += dx;
   y += dy;
-
 }
 
 setInterval(draw, 10);
---
+++ spec/exerciseSpec.js
@@ -0,0 +1,76 @@
+describe('array comprehension', () => {
+  /**
+   * Пример изменения каждого элемента
+   */
+  it('double array', () => {
+    function doubleIt(xs) {
+      res = [];
+      for (let i = 0; i < xs.length; i++) {
+        const x = xs[i];
+        res.push(x * 2);
+      }
+      return res;
+    }
+
+    a = [1, 2, 3];
+    exp = [2, 4, 6];
+    expect(doubleIt(a)).toEqual(exp);
+
+    exp1 = a.map(x => x * 2);
+    expect(doubleIt(a)).toEqual(exp1);
+  });
+
+  /**
+   * возведи каждый элемент в квадрат
+   */
+  it('square array', () => {
+    function square(xs) {
+      return xs.map(x => x ** 2);
+    }
+
+    a = [1, 2, 3];
+    exp = [1, 4, 9];
+    expect(square(a)).toEqual(exp);
+  });
+});
+
+describe('array filter', () => {
+  /**
+   * Пример проверки каждого элемента
+   */
+  it('check > 5', () => {
+    function checkMore5(xs) {
+      return xs.filter(x => x > 5);
+    }
+
+    a = [1, 6, 2, 7, 5];
+    exp = [6, 7];
+    expect(checkMore5(a)).toEqual(exp);
+  });
+
+  /**
+   * Отфильтруй все четные
+   */
+  it('firlter even', () => {
+    function evens(xs) {
+      return xs.filter(x => x % 2 == 0);
+    }
+
+    a = [1, 6, 2, 7, 5];
+    exp = [6, 2];
+    expect(evens(a)).toEqual(exp);
+  });
+
+  /**
+   * Отфильтруй все делящиеся на определенные числа
+   */
+  it('firlter div by numbers', () => {
+    function divby(xs, p, q) {
+      return xs.filter(x => x % (p * q) == 0);
+    }
+
+    a = [25, 15, 35, 30];
+    exp = [15, 30];
+    expect(divby(a, 3, 5)).toEqual(exp);
+  });
+});
