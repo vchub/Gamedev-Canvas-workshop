e74ff5db9d38dafeb4e2b965e1ae87568a7b264a
nnng <nnng@gmail.com>
2021-01-10 17:30:23 +0300

    describe

--- exercise/hero.test.js
+++ exercise/hero.test.js
@@ -2,20 +2,61 @@ var assert = require("chai").assert;
 
 // test example
 function id(x) {
-  return x;
+    return x;
 }
 
-describe("test example", () => {
-  it("id", () => {
-    assert.equal(id(1), 1);
-  });
+// id2 = (x) => { x }
+
+
+describe("test example", function () {
+    it("id", () => {
+        assert.equal(id(1), 1);
+    });
+
+    it("firstWord one word", () => {
+        assert.equal(0, 0);
+        var got = firstWord('hello')
+        var exp = "hello"
+        // debug print
+        // console.log('+++++++++ got:', got)
+        assert.equal(got, exp)
+        // or the same
+        assert.equal(firstWord('hello'), 'hello')
+    });
+
+    it("firstWord 2 word", () => {
+        assert.equal(firstWord('hello world'), 'hello')
+    });
+    
+    it("start with space", () => {
+        assert.equal(firstWord('   hello world'), 'hello')
+    });
+
+    it("normalize", () => {
+        var got = normalize('21-1')
+        console.log("++++++++++ got",got)
+        assert.equal(got,'21/1')
+
+        // assert.equal(normalize('21-1'),'21/1')
+    });
+    
+
 });
 // EDN test example
 
-function toCase(s) {
-  return s.toLowerCase() + "-" + s.toUpperCase();
+
+function firstWord(s) {
+    s = s.trim()
+    let i = s.indexOf(" ");
+    if (i <= -1) {
+        return s
+    }
+    return s.substr(0,i)
 }
 
-toCase("Mthatha");
-
-toCase("Mthatha") === "mthatha-MTHATHA";
+function normalize(s) {
+    while (s.indexOf("-")>= 0){
+        s = s.replace("-","/")
+    }
+    return s
+}
