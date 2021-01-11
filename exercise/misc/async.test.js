70853a8c6a1d4233a8d786336a024ce84c725bf2
vlad chub <vlad.chub@gmail.com>
2021-01-10 20:11:07 -0800

    add t.js as dummy. Promise.loop

--- exercise/misc/async.test.js
+++ exercise/misc/async.test.js
@@ -18,9 +18,30 @@ describe('promise', () => {
       .then(res => {
         // console.log('res', res);
         assert.deepEqual(res, [0, 1, 2]);
-        // done();
+        return res;
       });
 
+    // return p.should.eventually.deepEqual([1, 2, 3]);
     return p;
+  });
+
+  it('recursive loop', () => {
+    const N = 3;
+    const loop = ([xs, i]) => {
+      if (i && i >= N) {
+        return xs;
+      }
+      xs.push(i);
+      return loop([xs, i + 1]);
+    };
+    return new Promise((resolve, reject) => {
+      setTimeout(function() {
+        resolve([[], 0]);
+      }, 1);
+    })
+      .then(loop)
+      .then(xs => {
+        assert.deepEqual(xs, [0, 1, 2]);
+      });
   });
 });
